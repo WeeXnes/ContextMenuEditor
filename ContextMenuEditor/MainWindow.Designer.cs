7c97572c5b1113b9bae6f4e206f3bd5eeb79ed14
WeeXnes <somtrigi@gmail.com>
2022-06-11 16:55:26 +0200

    update

--- ContextMenuEditor/MainWindow.Designer.cs
+++ ContextMenuEditor/MainWindow.Designer.cs
@@ -31,11 +31,157 @@ namespace ContextMenuEditor
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_bgmenu = new System.Windows.Forms.RadioButton();
+            this.rb_foldermenu = new System.Windows.Forms.RadioButton();
+            this.rb_filemenu = new System.Windows.Forms.RadioButton();
+            this.tb_text = new System.Windows.Forms.TextBox();
+            this.tb_filepath = new System.Windows.Forms.TextBox();
+            this.tb_iconpath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_bgmenu);
+            this.groupBox1.Controls.Add(this.rb_foldermenu);
+            this.groupBox1.Controls.Add(this.rb_filemenu);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 91);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Type";
+            // 
+            // rb_bgmenu
+            // 
+            this.rb_bgmenu.Location = new System.Drawing.Point(6, 60);
+            this.rb_bgmenu.Name = "rb_bgmenu";
+            this.rb_bgmenu.Size = new System.Drawing.Size(165, 24);
+            this.rb_bgmenu.TabIndex = 2;
+            this.rb_bgmenu.TabStop = true;
+            this.rb_bgmenu.Text = "Background Menu";
+            this.rb_bgmenu.UseVisualStyleBackColor = true;
+            // 
+            // rb_foldermenu
+            // 
+            this.rb_foldermenu.Location = new System.Drawing.Point(6, 39);
+            this.rb_foldermenu.Name = "rb_foldermenu";
+            this.rb_foldermenu.Size = new System.Drawing.Size(104, 24);
+            this.rb_foldermenu.TabIndex = 1;
+            this.rb_foldermenu.TabStop = true;
+            this.rb_foldermenu.Text = "Folder Menu";
+            this.rb_foldermenu.UseVisualStyleBackColor = true;
+            // 
+            // rb_filemenu
+            // 
+            this.rb_filemenu.Location = new System.Drawing.Point(6, 19);
+            this.rb_filemenu.Name = "rb_filemenu";
+            this.rb_filemenu.Size = new System.Drawing.Size(104, 24);
+            this.rb_filemenu.TabIndex = 0;
+            this.rb_filemenu.TabStop = true;
+            this.rb_filemenu.Text = "File Menu";
+            this.rb_filemenu.UseVisualStyleBackColor = true;
+            // 
+            // tb_text
+            // 
+            this.tb_text.BackColor = System.Drawing.SystemColors.Window;
+            this.tb_text.Location = new System.Drawing.Point(94, 109);
+            this.tb_text.Name = "tb_text";
+            this.tb_text.Size = new System.Drawing.Size(242, 20);
+            this.tb_text.TabIndex = 1;
+            // 
+            // tb_filepath
+            // 
+            this.tb_filepath.Location = new System.Drawing.Point(94, 135);
+            this.tb_filepath.Name = "tb_filepath";
+            this.tb_filepath.Size = new System.Drawing.Size(242, 20);
+            this.tb_filepath.TabIndex = 2;
+            // 
+            // tb_iconpath
+            // 
+            this.tb_iconpath.Location = new System.Drawing.Point(94, 161);
+            this.tb_iconpath.Name = "tb_iconpath";
+            this.tb_iconpath.Size = new System.Drawing.Size(242, 20);
+            this.tb_iconpath.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Text:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ".exe path:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "icon path:";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(12, 187);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(324, 23);
+            this.btn_add.TabIndex = 7;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainWindow";
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(348, 223);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_iconpath);
+            this.Controls.Add(this.tb_filepath);
+            this.Controls.Add(this.tb_text);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Context Menu Editor";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_add;
+
+        private System.Windows.Forms.TextBox tb_text;
+        private System.Windows.Forms.TextBox tb_filepath;
+        private System.Windows.Forms.TextBox tb_iconpath;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_filemenu;
+        private System.Windows.Forms.RadioButton rb_foldermenu;
+        private System.Windows.Forms.RadioButton rb_bgmenu;
 
         #endregion
     }
--- ContextMenuEditor/MainWindow.cs
+++ ContextMenuEditor/MainWindow.cs
@@ -1,12 +1,124 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
 
 namespace ContextMenuEditor
 {
     public partial class MainWindow : Form
     {
+        public const string fileMenuRegistry = @"\*\shell";
+        public const string bgMenuRegistry = @"\Directory\Background\shell";
+        public const string folderMenuRegistry = @"\Directory\shell";
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            MenuType menu;
+            if (rb_filemenu.Checked)
+            {
+                menu = MenuType.FileMenu;
+            }
+            else if(rb_bgmenu.Checked)
+            {
+                menu = MenuType.BackgroundMenu;
+            }
+            else
+            {
+                menu = MenuType.FolderMenu;
+            }
+
+            if (!String.IsNullOrEmpty(tb_text.Text))
+            {
+                if (!String.IsNullOrEmpty(tb_filepath.Text))
+                {
+                    if (File.Exists(tb_filepath.Text))
+                    {
+                        if (!String.IsNullOrEmpty(tb_iconpath.Text))
+                        {
+                            if (File.Exists(tb_iconpath.Text))
+                            {
+                                writeToRegistry(menu, tb_text.Text, tb_filepath.Text, tb_iconpath.Text);
+                            }
+                            else
+                            {
+                                MessageBox.Show("The provided icon path does not exist");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Dont leave textbox \"icon path:\" empty");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("The provided .exe path does not exist");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Dont leave textbox \".exe path:\" empty");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dont leave textbox \"Text:\" empty");
+            }
+        }
+
+        public enum MenuType
+        {
+            FileMenu,
+            BackgroundMenu,
+            FolderMenu
+        }
+        public static void writeToRegistry(MenuType type, string text, string command, string icon)
+        {
+            string key;
+            string suffix;
+            
+            if (type == MenuType.FileMenu)
+            {
+                key = fileMenuRegistry;
+                suffix = "\"%1\"";
+            }
+            else if (type == MenuType.BackgroundMenu)
+            {
+                key = bgMenuRegistry;
+                suffix = "\"%V\"";
+            }
+            else if (type == MenuType.FolderMenu)
+            {
+                key = folderMenuRegistry;
+                suffix = "\"%V\"";
+            }
+            else
+            {
+                key = fileMenuRegistry;
+                suffix = "\"%1\"";
+            }
+
+            using (var shellKey = Registry.ClassesRoot.OpenSubKey(key, true))
+            {
+                shellKey.CreateSubKey(text);
+                using (var menuEntryKey = shellKey.OpenSubKey(text, true))
+                {
+                    menuEntryKey.SetValue("icon", icon);
+                    menuEntryKey.CreateSubKey("Command");
+                    using (var commandKey = menuEntryKey.OpenSubKey("Command", true))
+                    {
+                        commandKey.SetValue("", "\"" + command + "\" " + suffix);
+
+                    }
+                }
+
+            }
+        }
+        
     }
 }
