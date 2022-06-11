7faa96e8854d836adf119d8546b6a54b182a357d
WeeXnes <somtrigi@gmail.com>
2022-06-11 16:55:09 +0200

    Initial commit

---
+++ ContextMenuEditor/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ContextMenuEditor
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
